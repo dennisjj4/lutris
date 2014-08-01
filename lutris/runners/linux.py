69285eab8f2dbfa6f82e892f9da4b6319a87f3c3
Xodetaetl <dev@ovocean.com>
2014-08-01 15:50:28 +0200

    working_dir for Linux runner

--- lutris/runners/linux.py
+++ lutris/runners/linux.py
@@ -37,6 +37,29 @@ class linux(Runner):
         self.game_path = None
         self.settings = settings
 
+    @property
+    def game_exe_dir(self):
+        exe_path = self.settings['game'].get('exe') or None
+        if exe_path:
+            dir_path = os.path.dirname(exe_path)
+            if os.path.isabs(dir_path):
+                return dir_path
+            elif self.prefix_path:
+                return os.path.join(self.prefix_path, exe_path)
+
+    @property
+    def browse_dir(self):
+        """Returns the path to open with the Browse Files action."""
+        return self.working_dir  # exe path
+
+    @property
+    def working_dir(self):
+        """Return the working directory to use when running the game."""
+        if self.game_exe_dir:
+            return self.game_exe_dir
+        else:
+            return self.get_game_path()
+
     def is_installed(self):
         """Well of course Linux is installed, you're using Linux right ?"""
         return True
@@ -51,9 +74,6 @@ class linux(Runner):
         executable = game_config.get("exe")
         if not os.path.exists(executable):
             return {'error': 'FILE_NOT_FOUND', 'file': executable}
-
-        self.game_path = self.get_game_path()
-        launch_info['game_path'] = self.game_path
 
         ld_preload = game_config.get('ld_preload')
         if ld_preload:
